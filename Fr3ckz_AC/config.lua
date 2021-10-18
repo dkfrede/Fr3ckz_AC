23afa3b7e8ba2988c06190e2a8a4d225278b9118
Fr3ckzDK <65610875+Fr3ckz@users.noreply.github.com>
2021-10-18 08:44:32 +0200

    Update config.lua

--- Fr3ckz_AC/config.lua
+++ Fr3ckz_AC/config.lua
@@ -11,7 +11,7 @@
 
 Fr3ckzAC = {}
 
--- // DEFAULT // --
+-- // DEFAULT \\ --
 Fr3ckzAC.BlacklistWeapon = true -- Blacklisted Våben System
 Fr3ckzAC.DisableAllWeapons = false -- Blacklisted Alle våben
 Fr3ckzAC.BlacklistVehicle = true -- Blacklisted Køretøj System
@@ -22,23 +22,23 @@ Fr3ckzAC.TriggerProtection = true -- Blacklist Forskellige TriggerEvents
 Fr3ckzAC.AntiWord = true -- Blacklist af ord
 Fr3ckzAC.AntiSpectate = true -- Fjern muligheden for at spectate andre
 
--- // BAN/KICK-REASON // --
+-- // BAN/KICK-REASON \\ --
 Fr3ckzAC.BanReason = "Stop med at modde..." -- Ban/Kick grunden, når man bliver taget.
 
--- // EKSTRA // --
+-- // EKSTRA \\ --
 Fr3ckzAC.Permission = "player.kick" -- Hvilken "staff" permission, så at staff får de forskellige beskeder.
 Fr3ckzAC.AutoClear = true -- Fjern automatisk beskeder, hver stykke tid
 Fr3ckzAC.AutoClearDelay = "600000" -- Hvor langt skal der være imellem hver auto clear? (1000 = 1 sec)
 Fr3ckzAC.AutoMessage = true -- Send automatiske beskeder hvor mange der er blevet bannet af anticheatet imens servern har været oppe.
 Fr3ckzAC.AutoMessageDelay = "300000" -- Hvor langt skal der være imellem hver besked? (1000 = 1 sec)
 
--- // DISCORDLOG // --
+-- // DISCORDLOG \\ --
 Fr3ckzAC.Webhook = "INDSÆT HER" -- Dit webhook, så du får beskeder om hvem der modder og hvem der bliver taget.
 
--- // LICENSE // --
+-- // LICENSE \\ --
 Fr3ckzAC.License = "" -- Dit discord navn.
 
--- // WEAPON BLACKLIST // --
+-- // WEAPON BLACKLIST \\ --
 Fr3ckzAC.BlacklistedWeapons = { -- Blacklisted Våben
     "WEAPON_PUMPSHOTGUN",
     "WEAPON_RAYPISTOL",
@@ -53,7 +53,7 @@ Fr3ckzAC.BlacklistedWeapons = { -- Blacklisted Våben
     "WEAPON_MARKSMANRIFLE"
 }
 
--- // VEHICLE BLACKLIST // --
+-- // VEHICLE BLACKLIST \\ --
 Fr3ckzAC.BlacklistedVehicle = { -- Blacklisted Køretøjer
 	"RHINO",
 	"HYDRA",
@@ -61,19 +61,19 @@ Fr3ckzAC.BlacklistedVehicle = { -- Blacklisted Køretøjer
 	"JET"
 }
 
--- // PED BLACKLIST // --
+-- // PED BLACKLIST \\ --
 Fr3ckzAC.DefaultPed = "mp_m_freemode_01"
 Fr3ckzAC.BlacklistedPed = { -- Blacklisted Peds
     "a_m_m_beach_01"
 }
 
--- // TRIGGER PROTECTION // --
+-- // TRIGGER PROTECTION \\ --
 Fr3ckzAC.BlacklistedEvents = { -- Blacklisted Events
 	"adminmenu:allowall",
     "scrap:SellVehicle",
 }
 
--- // WORD BLACKLIST // --
+-- // WORD BLACKLIST \\ --
 Fr3ckzAC.AntiWords = { -- Blacklisted Ord
     "G0LD-K1DS",
 	"neger",
